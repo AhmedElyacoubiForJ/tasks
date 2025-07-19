3aaa5a85f0e153c08fe0b1706c4f9670e50618ae
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2025-07-19 15:15:47 +0200

    docs: document TaskListController endpoints with Swagger annotations and group under 'TaskLists' tag

--- src/main/java/edu/yacoubi/tasks/controllers/TaskListController.java
+++ src/main/java/edu/yacoubi/tasks/controllers/TaskListController.java
@@ -6,6 +6,7 @@ import edu.yacoubi.tasks.mappers.TaskListMapper;
 import edu.yacoubi.tasks.servcies.ITaskListService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "TaskLists", description = "Verwaltung von Task-Listen")
 @RestController
 @RequestMapping(path = "/task-lists")
 @RequiredArgsConstructor
